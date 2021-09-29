69e78a8bde06c473c28e378ec7fe1c0ed0111faa
rolaur <rolaur@ttu.ee>
2021-09-29 15:16:08 +0300

    Binary methods

--- hw01-ConsoleCalculator/ConsoleApp/Program.cs
+++ hw01-ConsoleCalculator/ConsoleApp/Program.cs
@@ -20,11 +20,11 @@ namespace ConsoleApp
             unary.Add(new MenuItem("D", "Absolute value", () => ""));   
             
             Menu binary = new(MenuLevel.Level1, "---- Level 1 - choose binary operation"); // +, -, /, *, x power y
-            binary.Add(new MenuItem("+", "Plus", Add)); 
-            binary.Add(new MenuItem("-", "Minus", () => "")); 
-            binary.Add(new MenuItem("/", "Divide", () => "")); 
-            binary.Add(new MenuItem("*", "Multiply", () => ""));
-            binary.Add(new MenuItem("P", "Power", () => ""));
+            binary.Add(new MenuItem("+", "Plus", Plus)); 
+            binary.Add(new MenuItem("-", "Minus", Minus)); 
+            binary.Add(new MenuItem("/", "Divide", Division)); 
+            binary.Add(new MenuItem("*", "Multiply", Multiply));
+            binary.Add(new MenuItem("P", "Power", Power));
             
             Menu menu = new(MenuLevel.Level0, "---- Level 0 - choose type");
             menu.Add(new MenuItem("A", "Binary", binary.Run));
@@ -32,13 +32,59 @@ namespace ConsoleApp
             menu.Run();
         }
         
-        public static string Add()
+        public static string Plus()
         {
             Console.Write($"Calculate: {CalculatorCurrentValue} + ");
             var n = Console.ReadLine()?.Trim();
             double.TryParse(n, out var converted);
             CalculatorCurrentValue = CalculatorCurrentValue + converted;
-            Console.Write($"Result: {CalculatorCurrentValue}");
+            Console.WriteLine($"Result: {CalculatorCurrentValue}");
+            Thread.Sleep(2000);
+            
+            return "";
+        }        
+        public static string Minus()
+        {
+            Console.Write($"Calculate: {CalculatorCurrentValue} - ");
+            var n = Console.ReadLine()?.Trim();
+            double.TryParse(n, out var converted);
+            CalculatorCurrentValue = CalculatorCurrentValue - converted;
+            Console.WriteLine($"Result: {CalculatorCurrentValue}");
+            Thread.Sleep(2000);
+            
+            return "";
+        }        
+        public static string Division()
+        {
+            Console.Write($"Calculate: {CalculatorCurrentValue} / ");
+            var n = Console.ReadLine()?.Trim();
+            double.TryParse(n, out var converted);
+            CalculatorCurrentValue = CalculatorCurrentValue / converted;
+            Console.WriteLine($"Result: {CalculatorCurrentValue}");
+            Thread.Sleep(2000);
+            
+            return "";
+        }
+        
+        public static string Multiply()
+        {
+            Console.Write($"Calculate: {CalculatorCurrentValue} * ");
+            var n = Console.ReadLine()?.Trim();
+            double.TryParse(n, out var converted);
+            CalculatorCurrentValue = CalculatorCurrentValue * converted;
+            Console.WriteLine($"Result: {CalculatorCurrentValue}");
+            Thread.Sleep(2000);
+            
+            return "";
+        }        
+        
+        public static string Power()
+        {
+            Console.Write($"Calculate: {CalculatorCurrentValue} power ");
+            var n = Console.ReadLine()?.Trim();
+            double.TryParse(n, out var converted);
+            CalculatorCurrentValue = Math.Pow(CalculatorCurrentValue, converted);
+            Console.WriteLine($"Result: {CalculatorCurrentValue}");
             Thread.Sleep(2000);
             
             return "";
