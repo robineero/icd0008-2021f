a2f4cee3c0f6c0553f48b749feaacbdd18b95de6
rolaur <rolaur@ttu.ee>
2021-10-11 13:49:13 +0300

    Config.cs

---
+++ hw03-BattleShipConsoleConfig/BattleShipBrain/Config.cs
@@ -0,0 +1,18 @@
+﻿namespace BattleShipBrain
+{
+    
+    // Configuration for game board
+    public class Config
+    {
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public bool Random { get; private set; }
+
+        public Config(int width, int height, bool random = false)
+        {
+            Width = width;
+            Height = height;
+            Random = random;
+        }
+    }
+}
